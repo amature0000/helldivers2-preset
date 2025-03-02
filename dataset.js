0ad5b150492000e794e5da0cb1d290a4890620bb
Juhyunsoo <amature0000@naver.com>
2025-03-03 00:09:43 +0900

    gas mine

--- dataset.js
+++ dataset.js
@@ -27,10 +27,9 @@ const imageMap = [
     {src :"assets/icons/Robotics Workshop/EMS Mortar Sentry.svg", caption : "EMS박격포", group: "group3"},
     {src :"assets/icons/Robotics Workshop/Mortar Sentry.svg", caption : "박격포", group: "group3"},
     {src :"assets/icons/Engineering Bay/Anti-Personnel Minefield.svg", caption : "지뢰", group: "group3"},
-    //{src :"assets/default.png"/*"assets/icons/_Experimental Stratagems/Gas Mine.svg"*/, caption : "가스지뢰", group: "group3"},
+    {src :"assets/temp/Gas Mine.svg", caption : "가스지뢰", group: "group3"},
     {src :"assets/icons/Engineering Bay/Incendiary Mines.svg", caption : "소이지뢰", group: "group3"},
     {src :"assets/icons/Engineering Bay/Anti-Tank Mines.svg", caption : "대전차지뢰", group: "group3"},
-    {src :"assets/default.png", caption : "", group: "group3"}, // padding
     {src :"assets/icons/Robotics Workshop/Gatling Sentry.svg", caption : "개틀링", group: "group3"},
     {src :"assets/icons/Robotics Workshop/Machine Gun Sentry.svg", caption : "기관총", group: "group3"},
     {src :"assets/icons/Robotics Workshop/Autocannon Sentry.svg", caption : "오토캐넌", group: "group3"},
@@ -140,5 +139,6 @@ const imageIndex = [
     "assets/icons/Urban Legends/Directional Shield.svg",
     "assets/icons/Engineering Bay/Shield Generator Pack.svg",
     "assets/icons/Patriotic Administration Center/StA-X3 W.A.S.P. Launcher.svg",
-    "assets/icons/Servants of Freedom/Hellbomb Portable.svg"
+    "assets/icons/Servants of Freedom/Hellbomb Portable.svg",
+    "assets/temp/Gas Mine.svg"
 ]
