a5f1eb067114cb7bf93f6541bcee4b28a7d5e3fc
Juhyunsoo <amature0000@naver.com>
2025-06-28 23:01:26 +0900

    new warbonds

--- dataset.js
+++ dataset.js
@@ -37,11 +37,12 @@ const imageMap = [
     {src :"assets/icons/Bridge/Shield Generator Relay.svg", caption : "실드릴레이", group: "group3"},
     {src :"assets/icons/Urban Legends/Flame Sentry.svg", caption : "화방", group: "group3"},
     {src :"assets/icons/Bridge/Tesla Tower.svg", caption : "테슬라", group: "group3"},
+    {src :"assets/icons/Bridge/Grenadier Battlement.svg", caption : "유탄방벽", group: "group3"},
     //==========================================================================================================================
     {src :"assets/icons/Robotics Workshop/Patriot Exosuit.svg", caption : "워커", group: "group4"},
     {src :"assets/icons/Robotics Workshop/Emancipator Exosuit.svg", caption : "오캐워커", group: "group4"},
     {src :"assets/icons/Hangar/Fast Recon Vehicle.svg", caption: "정찰차", group: "group4"},
-    {src :"assets/default.png", caption : "", group: "group4"}, // padding
+    {src :"assets/icons/Masters of Ceremony/One True Flag.svg", caption : "깃발", group: "group4"},
     {src :"assets/icons/Patriotic Administration Center/Stalwart.svg", caption : "스털워트", group: "group4"},
     {src :"assets/icons/Patriotic Administration Center/Machine Gun.svg", caption : "기관총", group: "group4"},
     {src :"assets/icons/Patriotic Administration Center/Heavy Machine Gun.svg", caption : "중기관총", group: "group4"},
@@ -54,6 +55,7 @@ const imageMap = [
     {src :"assets/icons/Patriotic Administration Center/Flamethrower.svg", caption : "화염방사기", group: "group4"},
     {src :"assets/icons/Chemical Agents/Sterilizer.svg", caption : "가스방사기", group: "group4"},
     {src :"assets/icons/Engineering Bay/Arc Thrower.svg", caption : "아크발사기", group: "group4"},
+    {src :"assets/icons/Force of Law/GL-52 De-Escalator.svg", caption : "아크유탄", group: "group4"},
     {src :"assets/icons/Patriotic Administration Center/Anti-Materiel Rifle.svg", caption : "대물저격", group: "group4"},
     //==========================================================================================================================
     {src :"assets/icons/Patriotic Administration Center/Airburst Rocket Launcher.svg", caption : "공폭발사기", group: "group5"},
@@ -64,16 +66,17 @@ const imageMap = [
     {src :"assets/default.png", caption : "", group: "group5"}, // padding
     {src :"assets/default.png", caption : "", group: "group5"},
     {src :"assets/default.png", caption : "", group: "group5"},
-    {src :"assets/icons/Engineering Bay/Supply Pack.svg", caption : "보급팩", group: "group5"},
     {src :"assets/icons/Robotics Workshop/Guard Dog.svg", caption : "가드독", group: "group5"},
     {src :"assets/icons/Engineering Bay/Guard Dog Rover.svg", caption : "가드독:로버", group: "group5"},
     {src :"assets/icons/Chemical Agents/Guard Dog Breath.svg", caption : "가드독:가스", group: "group5"},
+    {src :"assets/icons/Force of Law/Guard Dog K-9.svg", caption : "가드독:K9", group: "group5"},
+    {src :"assets/icons/Engineering Bay/Supply Pack.svg", caption : "보급팩", group: "group5"},
     {src :"assets/icons/Servants of Freedom/Hellbomb Portable.svg", caption : "핵가방", group: "group5"},
+    {src :"assets/icons/Hangar/Jump Pack.svg", caption : "점프팩", group: "group5"},
+    {src :"assets/icons/Borderline Justice/Hover Pack.svg", caption : "호버팩", group: "group5"},
     {src :"assets/icons/Engineering Bay/Ballistic Shield Backpack.svg", caption : "방패", group: "group5"},
     {src :"assets/icons/Urban Legends/Directional Shield.svg", caption : "에너지방패", group: "group5"},
     {src :"assets/icons/Engineering Bay/Shield Generator Pack.svg", caption : "실드팩", group: "group5"},
-    {src :"assets/icons/Hangar/Jump Pack.svg", caption : "점프팩", group: "group5"},
-    {src :"assets/icons/Borderline Justice/Hover Pack.svg", caption : "호버팩", group: "group5"},
 ];
 
 const imageIndex = [
@@ -142,5 +145,9 @@ const imageIndex = [
     "assets/icons/Patriotic Administration Center/StA-X3 W.A.S.P. Launcher.svg",
     "assets/icons/Servants of Freedom/Hellbomb Portable.svg",
     "assets/icons/Engineering Bay/Gas Mine.svg",
-    "assets/icons/Borderline Justice/Hover Pack.svg"
+    "assets/icons/Borderline Justice/Hover Pack.svg",
+    "assets/icons/Bridge/Grenadier Battlement.svg",
+    "assets/icons/Force of Law/GL-52 De-Escalator.svg",
+    "assets/icons/Force of Law/Guard Dog K-9.svg",
+    "assets/icons/Masters of Ceremony/One True Flag.svg"
 ]
